8ce88fcb66d717e25b191691bea816c7f5e6cbc3
MGutix <140160200+MGutix@users.noreply.github.com>
2023-10-24 17:18:41 +0900

    MVP (no edit)

--- src/index.js
+++ src/index.js
@@ -1,22 +1,86 @@
-import createTodo from "./todo"
+import Todo from "./todo"
 
+const addButton = document.getElementById('add')
+let modal = document.querySelector('[data-modal]')
+let submit = document.getElementById('submit')
 let todoArray = [];
 
 
-const addButton = document.getElementById('add')
+function openModal(){
+        modal.showModal();
 
-addButton.addEventListener('click', ()=>{
-    //agregar modal que pide info
-    //pasar info como parametros a la fabrica
+        //para que no queden residuos en el modal cuando haces uno nuevo
+        let inputs = document.querySelectorAll('input') 
+        inputs.forEach(input => {
+            input.value = '';
+        });
+      
+      
+        //buscar submit
+      submit.addEventListener('click', createTodo)
+    
+}
 
-    const todo = createTodo ('hacer compras', 'pollo', 'hoy', 1)
+function createTodo(){
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let dueDate = document.getElementById('dueDate').value;
+    let priority = document.getElementById('priority').value;
 
-    console.log(todo)
-    todo.edit()
-    todo.delete()
-
+    const todo = new Todo (title, description, dueDate, priority)
     todoArray.push(todo)
 
-    console.log(todoArray)
-})
+    console.table(todoArray)
+
+    renderArray()
+}
+
+function renderArray() {
+    let todoArea = document.getElementById('todoArea');
+    //borra todo el contenido para no duplicar
+    todoArea.innerHTML = ''
+    
+    todoArray.map((element) =>{
+        let todoCard = document.createElement('div')
+        todoCard.classList.add('card-body')
+        todoCard.classList.add('card')
+        todoArea.appendChild(todoCard)
+
+        let cardTitle = document.createElement('h3')
+        cardTitle.textContent = element.title
+        cardTitle.classList.add('card-title')
+        todoCard.appendChild(cardTitle)
+
+        let cardDesc = document.createElement('p')
+        cardDesc.textContent = element.description
+        cardDesc.classList.add('card-text')
+        todoCard.appendChild(cardDesc)
+
+        let cardDate = document.createElement('p')
+        cardDate.textContent = element.dueDate
+        todoCard.appendChild(cardDate)
+
+        let cardPriority = document.createElement('p')
+        cardPriority.textContent = element.priority
+        todoCard.appendChild(cardPriority)
+
+        let cardDelete = document.createElement('button')
+        cardDelete.textContent = 'Delete'
+        todoCard.appendChild(cardDelete)
+
+        cardDelete.addEventListener('click', () => {
+      
+            todoArray.splice(element,1);
+            todoCard.remove();
+            console.log(todoArray)
+          })
+    })
+}
+
+function toggle(){
+    //event listener on button + vincular al objeto correcto
+    todo.toggleIsDone()
+}
+
+addButton.addEventListener('click', () =>{openModal()})
 
--- src/todo.js
+++ src/todo.js
@@ -1,32 +1,33 @@
-function createTodo (title, description, dueDate, priority) {
-    return {    
-        title: title,
-        description: description,
-        dueDate: dueDate,
-        priority: priority,
+class Todo {
+    constructor(title, description, dueDate, priority) {
 
-        //empieza en false, si le hacen click pasa a true y se va a completados
-        isDone: false,
+        this.title = title
+        this.description = description
+        this.dueDate = dueDate
+        this.priority = priority
 
-        //methods
-        edit:function(){
-            //cambiar
-            console.log('im editting')
-        },
-        delete:function(){
-            //cambiar
-            console.log('im deleteting')
-        },
+        //empieza en false si le hacen click pasa a true y se va a completados
+        this.isDone = false
     }
 
-    
-  }
+    edit() {
+        //cambiar
+        console.log('im editting')
+    }
+    delete() {
+        //cambiar
+        console.log('im deleteting')
+    }
 
-  
-/*const todo1 = createTodo ('hacer compras', 'pollo', 'hoy', 1)
+    toggleIsDone() {
+        if (this.isDone === false) {
+            this.isDone = true
+        } else {
+            this.isDone = false
+        }
+        //cambiar
+        console.log(`isDone toggled to ${this.isDone}`)
+    }
+}
 
-console.log(todo1)
-todo1.edit()
-todo1.delete()*/
-
-export default createTodo;
+export default Todo;
